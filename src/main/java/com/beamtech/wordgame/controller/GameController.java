78ec0ad2ba3afae085a099ecde33bf919d0c0e43
burak.senel <burak.senel@beamteknoloji.com>
2020-08-14 15:05:13 +0300

    test12

--- src/main/java/com/beamtech/wordgame/controller/GameController.java
+++ src/main/java/com/beamtech/wordgame/controller/GameController.java
@@ -1,9 +1,7 @@
 package com.beamtech.wordgame.controller;
 
 import com.beamtech.wordgame.dto.GameDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
