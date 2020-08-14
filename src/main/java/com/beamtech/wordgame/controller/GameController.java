121c4ae5703be255faae3f2f59abe99730597470
burak.senel <burak.senel@beamteknoloji.com>
2020-08-14 15:38:23 +0300

    test12

--- src/main/java/com/beamtech/wordgame/controller/GameController.java
+++ src/main/java/com/beamtech/wordgame/controller/GameController.java
@@ -1,25 +1,11 @@
 package com.beamtech.wordgame.controller;
 
-import com.beamtech.wordgame.dto.GameDto;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.beamtech.wordgame.model.GenericResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("game")
 public class GameController {
-
-    public List<GameDto> games = new ArrayList<GameDto>();
-
-
-
-
-    @GetMapping("active-games")
-    public List<GameDto> activeGames(){
-        return games;
-    }
 
 }
