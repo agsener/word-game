c3ce9b94f4b5ce3696749fb8bcd7e0449ae51dc7
ahmet.sener <gokhan.sener@beamteknoloji.com>
2020-08-20 16:07:17 +0300

    test.

--- src/main/resources/static/app/js/game/game.component.js
+++ src/main/resources/static/app/js/game/game.component.js
@@ -13,22 +13,7 @@ angular.module("game")
                             //kazandi
                             //tekrar oynamak istermisin
                             alert("Kazandin: " + response.winner);
-                            console.log("Kazandin: " + response.winner);
-                            Swal.fire({
-                                title: 'Winner!',
-                                text: ' Are play again?',
-                                icon: 'info',
-                                showCancelButton: true,
-                                confirmButtonColor: '#3085d6',
-                                cancelButtonColor: '#d33',
-                                confirmButtonText: 'Yes, do it again!'
-                            }).then(function (result) {
-                                if (result.value) {
-                                    alert("Tekrar oy ist")
-                                } else {
-                                    $location.path("/lobby");
-                                }
-                            });
+                            $location.path("/lobby");
                         } else if (rsp.username === response.whosTurn) {
                             $scope.isMyTurn = true;
                             $scope.whosTurn = response.whosTurn;
@@ -55,24 +40,8 @@ angular.module("game")
                             //kaybetti
                             //tekrar oynamak istermisin
                             alert("Kaybettin: " + rsp.username);
-                            console.log("Kaybettin: " + rsp.username);
-                            Swal.fire({
-                                title: 'Loser!',
-                                text: ' Are play again?',
-                                icon: 'warning',
-                                showCancelButton: true,
-                                confirmButtonColor: '#3085d6',
-                                cancelButtonColor: '#d33',
-                                confirmButtonText: 'Yes, try again!'
-                            }).then(function (result) {
-                                if (result.value) {
-                                    alert("Tekrar oy ist")
-                                } else {
-                                    $location.path("/lobby");
-                                }
-                            });
-                        }
-                        else if (rsp.username === response.whosTurn) {
+                            $location.path("/lobby");
+                        } else if (rsp.username === response.whosTurn) {
                             whosTurn = response.whosTurn;
                             $scope.isMyTurn = true;
                         } else {
